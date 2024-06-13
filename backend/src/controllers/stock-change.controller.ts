293b503cd2216f241a2f1d9c6ac814b4892b32f9
julien-rtg <julien.rittl@gmail.com>
2024-06-13 09:18:25 +0200

    order desc movement

--- backend/src/controllers/stock-change.controller.ts
+++ backend/src/controllers/stock-change.controller.ts
@@ -3,7 +3,7 @@ import { StockChange } from "../models/index.ts";
 export class StockChangeController {
   public async getStockChanges(offset: number, limit: number) {
     try {
-      const stockChanges = await StockChange.findAndCountAll({offset: offset, limit: limit});
+      const stockChanges = await StockChange.findAndCountAll({offset: offset, limit: limit, order: [["createdAt", "DESC"]]});
       return { message: stockChanges, code: 200 };
     } catch (error) {
       return { message: error, code: 500 };
